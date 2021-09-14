33096377ae3eb33791edf7d01e0f3899ce21762e
eduard <44374669+eduardevdokimov746@users.noreply.github.com>
2021-09-14 11:21:44 +0300

    Update sockets.php

--- sockets.php
+++ sockets.php
@@ -3,7 +3,14 @@
 return [
     'port' => env('SOCKET_PORT', 6001),
 
+    'defaultChannel' => 'main',
 
+    'components' => [
+        'publisher' => \App\Sockets\SocketPublisher::class,
+        'channels' => \App\Sockets\SocketChannels::class,
+        'listener' => \App\Sockets\MessageListener::class,
+        'worker' => \App\Sockets\SocketWorker::class
+    ],
 
     'actions' => [
         'login' => \App\Sockets\Actions\LoginAction::class
